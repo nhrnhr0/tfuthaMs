971ab0f5bf4443008ce5cfcb32496c5d0f1fe486
nhrnhr0 <nhrnhr0@gmail.com>
2022-01-12 06:06:20 +0200

    adding ack support, need to check acking only sent messages

---
+++ README.md
@@ -0,0 +1,5 @@
+# setup
+git clone https://github.com/nhrnhr0/tfuthaMs.git
+cd tfuthaMs
+npm i
+npm run start
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client } = require('whatsapp-web.js');
+const { Client, MessageAck } = require('whatsapp-web.js');
 require('dotenv').config();
 
 const TelegramBot = require('node-telegram-bot-api');
@@ -7,6 +7,13 @@ const qr = require('qr-image');
 const client = new Client({ puppeteer: { headless: false }, clientId: 'example' });
 const TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
 const TELEGRAM_CHAT_ID = process.env.TELEGRAM_CHAT_ID;
+
+let sent_unread_messages = [];
+
+if(!TELEGRAM_BOT_TOKEN || !TELEGRAM_CHAT_ID) {
+    console.error('Please set TELEGRAM_BOT_TOKEN and TELEGRAM_CHAT_ID in .env');
+    process.exit(1);
+}
 const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, {polling: true});
 let whatsapp_bot_status = "offline";
 
@@ -25,7 +32,28 @@ client.on('qr', (qr_code) => {
     
     
 });
-
+client.on("message_ack", async (msg, ack) => {
+    console.log('message_ack', msg, ack);
+    console.log(msg); 
+    console.log(ack);
+    let ack_str = '?';
+    
+    if(ack == MessageAck.ACK_ERROR) {
+        ack_str = 'error';
+    }else if(ack == MessageAck.ACK_PENDING) {
+        ack_str = 'pending';
+    }else if(ack == MessageAck.ACK_SERVER) {
+        ack_str = 'server';
+    }else if(ack == MessageAck.ACK_READ) {
+        ack_str = 'read';
+    }else if(ack == MessageAck.ACK_DEVICE) {
+        ack_str = 'device';
+    }else if(ack == MessageAck.ACK_PLAYED) {
+        ack_str = 'played';
+    }
+    
+    bot.sendMessage(TELEGRAM_CHAT_ID, `${msg.from.replace('@c.us', '')}:\n${msg.body}\nACK: ${ack_str}`);
+})
 client.on('ready', () => {
     whatsapp_bot_status = 'ready';
     console.log('Client is ready!');
@@ -34,25 +62,48 @@ client.on('ready', () => {
 
 bot.on('message', (msg) => {
     console.log('message', msg);
-    if (msg.text === '/restart') {
-        bot.sendMessage(msg.chat.id, 'התחל מחדש בעוד 5 שניות');
-        setTimeout(()=> {
-            process.exit(0);
-        },5000);
-        
-    }else if(msg.text === '/status') {
-        bot.sendMessage(msg.chat.id, whatsapp_bot_status);
-    }else if(msg.text.startsWith('/lead')){
-        let number = msg.text.substring(6);
-        console.log('lead', number);
-        const bot_number = '+972555571040';
-        let contact_id = bot_number.substring(1) + "@c.us";
-        let contact = client.getContactById(contact_id).then(contact => {
-            const chatId = number.substring(1) + "@c.us";
-            let text = 'אנחנו שמחים שהחלטת להצטרף לרשימת הVIP שלנו. אנא שמור מספר זה'
-            client.sendMessage(chatId, text);
-            client.sendMessage(chatId, contact);
-        });
+    if(msg.chat.id == TELEGRAM_CHAT_ID){
+        if (msg.text === '/restart') {
+            bot.sendMessage(msg.chat.id, 'התחל מחדש בעוד 5 שניות');
+            setTimeout(()=> {
+                process.exit(0);
+            },5000);
+            
+        }else if(msg.text === '/status') {
+            bot.sendMessage(msg.chat.id, whatsapp_bot_status);
+        }else if(msg.text.startsWith('/lead')){
+            let number = msg.text.substring(6);
+            if(number.startsWith('+') && number.length == 13) {
+                number = number.substring(1);
+            }else if(number.startsWith('0') && number.length == 10){
+                number = '972' + number.substring(1);
+            }else {
+                bot.sendMessage(msg.chat.id, number + ' לא תקין');
+                return;
+            }
+            console.log('lead', number);
+            const bot_number = '+972555571040';
+            let contact_id = bot_number.substring(1) + "@c.us";
+            let contact = client.getContactById(contact_id).then(contact => {
+                const chatId = number + "@c.us";
+                let text = 'אנחנו שמחים שהחלטת להצטרף לרשימת הVIP שלנו. אנא שמור מספר זה'
+                let text_response = client.sendMessage(chatId, text);
+                let contact_response = client.sendMessage(chatId, contact);
+                text_response.then((text_resp)=> {
+                    contact_response.then((contact_resp)=> {
+                        sent_unread_messages.push({
+                            message_id: text_resp.id,
+                        });
+                        sent_unread_messages.push({
+                            message_id: contact_resp.id,
+                        });
+
+                        bot.sendMessage(msg.chat.id, 'הודעה נשלחה ל' + number);
+                    });
+                });
+                
+            });
+        }
     }
 });
 
